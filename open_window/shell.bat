6bb371240b7bfacfb6e486f4df508672d9daecc5
Anton Kholetsky <kvazipolyr@gmail.com>
2019-09-05 18:13:28 +0300

    Update shell.bat

--- open_window/shell.bat
+++ open_window/shell.bat
@@ -6,7 +6,8 @@ cd /d %scriptsDir%
  
 :: uncomment the line below line to debug the vcvars
 :: set VSCMD_DEBUG=1
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+:: call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
  
 set path=%scriptsDir%;%path%
  
