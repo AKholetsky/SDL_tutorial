99346405bae6b849f63ba96be2deb1728f3da5d8
Anton Kholetsky <anton.kholetskiy@hiqo-solutions.com>
2019-09-05 19:37:09 +0300

    Actions settings updated

--- open_window/shell.bat
+++ open_window/shell.bat
@@ -2,12 +2,12 @@
 echo started
 
 set scriptsDir=%~dp0
-echo %scriptsDir%
 cd /d %scriptsDir%
  
 :: uncomment the line below line to debug the vcvars
 :: set VSCMD_DEBUG=1
 :: call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+:: for github actions windows-2016
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
  
 set path=%scriptsDir%;%path%
